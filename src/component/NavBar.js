ec77304abbe5a8f421146cf9e19a02a0d2a5c0d5
yanis bekhtaoui <yanis.bekhtaoui@gmail.com>
2020-03-20 16:38:57 -0700

    nav bar almost done

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -11,24 +11,29 @@ export default class NavBar extends Component {
     render() {
         const { isOpen } = this.state;
         return (
-                <div className={isOpen ?'clickNavBar-container':'navBar-container'}>
-                    <div className={isOpen ?'clickName-container':'name-container'}>
-                        <h1 className={isOpen ?'clickName':'Name'}>Yanis bekbek</h1>
+            <div className="nav">
+                <div className="nav-container">
+                    <div className="name-container">
+                        <h1 className={isOpen ?'nameOpen' :'nameClose'}>Yanis B</h1>
                     </div>
-                        <div className="hamburger" onClick={this.handleMenuButtonClick}>
-                            <div className="line"></div>
-                            <div className="line"></div>
-                            <div className="line"></div>
+                    <div className="hamburger" onClick={this.handleMenuButtonClick}>
+                        <div className={isOpen ? 'cross':'btn'}></div>
                     </div>
-                    <div className={isOpen ?'showContainer' :'hideContainer'}>
-                        <ul className={isOpen ? 'showItem' : 'hide'}>
-                            <li><a href='#'>About</a></li>
-                            <li><a href='#'>Contact</a></li>
-                        </ul>
-
+                    <div className={isOpen ? 'play' :'close'}>
+                        <li><a href="#" className={isOpen ? 'playTwo' :'closeTwo'}>Home</a></li>
+                        <li><a href="#" className={isOpen ? 'playTwo' :'closeTwo'}>About</a></li>
+                        <li><a href="#"className={isOpen ? 'playTwo' :'closeTwo'}>Contact</a></li>
+                    </div>
+                    <div className="menu-desktop">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Contact</a></li>
                     </div>
 
                 </div>
+            </div>
+
         )
     }
 }
+
